0e15f9c74c11163ffd0b8513563f6589bbb2b8af
wojiaozhupengfei <810020522@qq.com>
2018-11-24 12:40:42 +0800

    modify

--- leetcode/leetcode/67.py
+++ leetcode/leetcode/67.py
@@ -52,14 +52,15 @@ class Sulotion:
         # sum = Binary2Decimal(a) + Binary2Decimal(b)
         # ret = Decimal2Binary(sum)
         # return ret
-        res = ''
-        i, j, plus = len(a) - 1, len(b) - 1, 0
-        while i >= 0 or j >= 0 or plus == 1:
-            plus += int(a[i]) if i >= 0 else 0
-            plus += int(b[j]) if j >= 0 else 0
-            res = str(plus % 2) + res
-            i, j, plus = i - 1, j - 1, plus / 2
-        return res
+
+        # res = ''
+        # i, j, plus = len(a) - 1, len(b) - 1, 0
+        # while i >= 0 or j >= 0 or plus == 1:
+        #     plus += int(a[i]) if i >= 0 else 0
+        #     plus += int(b[j]) if j >= 0 else 0
+        #     res = str(plus % 2) + res
+        #     i, j, plus = i - 1, j - 1, plus / 2
+        # return res
 
 # a = '111'
 # b = 11
